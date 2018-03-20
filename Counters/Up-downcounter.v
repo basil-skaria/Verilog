da1f46475ad788bbbe43055c6a8a73458c97f2fe
Basil Skaria <31174477+basil-skaria@users.noreply.github.com>
2018-03-20 21:24:46 +0530

    Create Up-downcounter.v

---
+++ Counters/Up-downcounter.v
@@ -0,0 +1,36 @@
+module updown(clk,clr,up,q);
+input clk,clr,up;
+output [3:0] q;
+reg [3:0] q;
+
+always@(posedge clear or posedge clk)
+
+begin
+  if(clear)
+    q <=4’b0000;
+  else if(up)
+    q <= q+1’b1;
+  else
+    q <= q-1’b1;
+end
+endmodule
+
+module tb;
+
+reg clk;
+reg clr;
+reg up;
+wire [3:0] q;
+updown uut (.clk(clk),.clr(clr),.up(up),.q(q));
+
+initial begin
+clk = 0;
+clr = 0;
+up = 0;
+#5 clr=1’b1;
+#5 clr=1’b0;
+#100 up=1’b1;
+end
+always #5 clk=~clk;
+initial #150 $stop;
+endmodule

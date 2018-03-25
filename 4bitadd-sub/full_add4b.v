455cbff69346bfcf67b1d37894748894ab917768
Basil Skaria <31174477+basil-skaria@users.noreply.github.com>
2018-03-25 10:11:24 +0530

    Update full_add4b.v

--- 4bitadd-sub/full_add4b.v
+++ 4bitadd-sub/full_add4b.v
@@ -1,21 +1,20 @@
-   
-module full_sub ( a ,b ,c ,sum,carry );
+module full_add ( a ,b ,c ,sum1,carry );
 
-output sum ;
+output sum1 ;
 output carry ;
 
 input a ;
 input b ;
 input c ;
    
-assign diff = a ^ b ^ c;   
-assign borrow = ((a)&b) | (b&c) | (c&(a));
+assign sum1 = a ^ b ^ c;   
+assign carry = ((a)&b) | (b&c) | (c&(a));
 
 endmodule
 
-module sub_4bit ( a ,b ,carry ,sum );
+module add_4bit ( a ,b ,carry ,sum1 );
 
-output [3:0] sum ;
+output [3:0] sum1 ;
 output carry;
 
 input [3:0] a ;
@@ -23,10 +22,10 @@ input [3:0] b ;
 
 wire [2:0]s;
 
-full_add u0 (a[0],b[0],1'b0,sum[0],s[0]);
-full_add u0 (a[1],b[1],1'b0,sum[0],s[1]);
-full_add u0 (a[2],b[2],1'b0,sum[0],s[2]);
-full_add u0 (a[3],b[3],1'b0,sum[0],carry);
+full_add u0 (a[0],b[0],1'b0,sum1[0],s[0]);
+full_add u1 (a[1],b[1],s[0],sum1[1],s[1]);
+full_add u2 (a[2],b[2],s[1],sum1[2],s[2]);
+full_add u3 (a[3],b[3],s[2],sum1[3],carry);
 
 endmodule
 
@@ -36,92 +35,19 @@ module tb_full_add;
      reg [3:0]a;
 	 reg [3:0]b;
 	 reg cin;
-     wire [3:0]sum;
-	 wire [3:0]cout;
-sub_4bit uut (.a(a),.b(b),.sum(sum),.carry(carry));
+     wire [3:0]sum1;
+	 wire carry;
+add_4bit uut (.a(a),.b(b),.sum1(sum1),.carry(carry));
      
 initial begin
 			 $dumpfile("Full_sub.vcd");
 		 $dumpvars;
-$display("  a   |  b   | sum | borrow");		
-$monitor(" %b | %b | %b  | %b ",a,b,sum,carry);
+$display("  a   |  b   | sum | carry");		
+$monitor(" %b | %b | %b  | %b ",a,b,sum1,carry);
 a = 0; b = 0; #5
-a = 0; b = 1; #5
-a = 0; b = 2; #5
-a = 0; b = 3; #5
-a = 0; b = 4; #5
-a = 0; b = 5; #5
-a = 0; b = 6; #5
-a = 0; b = 7; #5
-
-a = 1; b = 0; #5
-a = 1; b = 1; #5
-a = 1; b = 2; #5
-a = 1; b = 3; #5
-a = 1; b = 4; #5
-a = 1; b = 5; #5
-a = 1; b = 6; #5
-a = 1; b = 7; #5
-
-a = 2; b = 0; #5
-a = 2; b = 1; #5
-a = 2; b = 2; #5
-a = 2; b = 3; #5
-a = 2; b = 4; #5
-a = 2; b = 5; #5
-a = 2; b = 6; #5
-a = 2; b = 7; #5
-
-a = 3; b = 0; #5
-a = 3; b = 1; #5
-a = 3; b = 2; #5
-a = 3; b = 3; #5
-a = 3; b = 4; #5
-a = 3; b = 5; #5
-a = 3; b = 6; #5
-a = 3; b = 7; #5
-
-a = 4; b = 0; #5
-a = 4; b = 1; #5
-a = 4; b = 2; #5
-a = 4; b = 3; #5
-a = 4; b = 4; #5
-a = 4; b = 5; #5
-a = 4; b = 6; #5
-a = 4; b = 7; #5
-a = 4; b = 8; #5
-a = 4; b = 9; #5
-
-a = 5; b = 0; #5
-a = 5; b = 1; #5
-a = 5; b = 2; #5
-a = 5; b = 3; #5
-a = 5; b = 4; #5
-a = 5; b = 5; #5
-a = 5; b = 6; #5
-a = 5; b = 7; #5
-a = 5; b = 8; #5
-a = 5; b = 9; #5
-a = 6; b = 0; #5
-a = 6; b = 1; #5
-a = 6; b = 2; #5
-a = 6; b = 3; #5
-a = 6; b = 4; #5
-a = 6; b = 5; #5
-a = 6; b = 6; #5
-a = 6; b = 7; #5
-a = 6; b = 8; #5
-a = 6; b = 9; #5
-a = 7; b = 0; #5
-a = 7; b = 1; #5
-a = 7; b = 2; #5
-a = 7; b = 3; #5
-a = 7; b = 4; #5
-a = 7; b = 5; #5
-a = 7; b = 6; #5
-a = 7; b = 7; #5
-a = 7; b = 8; #5
-a = 7; b = 9; 
+a = 9; b = 2; #5
+a = 8; b = 5; #5
+a = 9; b = 9;
 
 end
  

2ffbc520212347b1de5485e3ca88a6d20ffbd0b6
Basil Skaria <31174477+basil-skaria@users.noreply.github.com>
2018-03-25 10:47:55 +0530

    Update sipo.v

--- SIPO/sipo.v
+++ SIPO/sipo.v
@@ -1,3 +1,10 @@
+/////////////////////////////////////////
+// Basil Skaria (20)                   // 
+// S4 CSE                              //
+// MITS                                // 
+// Verilog codes of lab cycle          //
+//                                     //
+/////////////////////////////////////////
 module SIPO ( din ,clk ,reset ,dout );
 
 output [3:0] dout ;

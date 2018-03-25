7b0f27fe9f1d02bdd13259a481d927d5f98f5bfa
Basil Skaria <31174477+basil-skaria@users.noreply.github.com>
2018-03-25 10:50:38 +0530

    Update jkff.v

--- fundamental flipflops/jkff.v
+++ fundamental flipflops/jkff.v
@@ -1,3 +1,10 @@
+/////////////////////////////////////////
+// Basil Skaria (20)                   // 
+// S4 CSE                              //
+// MITS                                // 
+// Verilog codes of lab cycle          //
+//                                     //
+/////////////////////////////////////////
 module jkff ( j ,k ,clk ,cl ,q ,nq );
 
 output q ;
